12fbe79f8bcbe6606bfa22e47665dbb4b973eb3d
AashrithG <gummadiaashrith@gmail.com>
2025-07-10 19:48:19 -0400

    Setup swerve drive
    Setup mechanical advantage swerve template, then generated turner constants

--- 2025-SummerActivity/build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ 2025-SummerActivity/build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-SummerActivity";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "cf656c596d6bd7a138553186960997c5e67e1046";
-  public static final String GIT_DATE = "2025-07-07 22:01:23 EDT";
-  public static final String GIT_BRANCH = "SwerveTemplate";
-  public static final String BUILD_DATE = "2025-07-10 16:59:26 EDT";
-  public static final long BUILD_UNIX_TIME = 1752181166919L;
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "e4aeae3aeb3729db206a0aa8bbfe6535e5b6230e";
+  public static final String GIT_DATE = "2025-07-10 19:10:55 EDT";
+  public static final String GIT_BRANCH = "aashriths_branch";
+  public static final String BUILD_DATE = "2025-07-10 19:18:16 EDT";
+  public static final long BUILD_UNIX_TIME = 1752189496361L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
