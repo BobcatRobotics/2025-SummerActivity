a5296aa96b5451c831448fe554383d34b7133166
GIRPRA <girishprasad102@gmail.com>
2025-07-15 19:14:27 -0400

    Girish and Alex made some edits to checkstyles.

--- 2025-SummerActivity/src/main/java/frc/robot/BuildConstants.java
+++ 2025-SummerActivity/src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-SummerActivity";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "cf656c596d6bd7a138553186960997c5e67e1046";
-  public static final String GIT_DATE = "2025-07-07 22:01:23 EDT";
-  public static final String GIT_BRANCH = "SwerveTemplate";
-  public static final String BUILD_DATE = "2025-07-10 17:03:51 EDT";
-  public static final long BUILD_UNIX_TIME = 1752181431805L;
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "6c0e388e1568a96a8981e368009e26756b17ad9f";
+  public static final String GIT_DATE = "2025-07-10 19:36:04 EDT";
+  public static final String GIT_BRANCH = "Girish_Branch";
+  public static final String BUILD_DATE = "2025-07-15 18:44:10 EDT";
+  public static final long BUILD_UNIX_TIME = 1752619450389L;
   public static final int DIRTY = 1;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
