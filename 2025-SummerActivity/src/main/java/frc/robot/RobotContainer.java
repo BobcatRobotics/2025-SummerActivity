e9c7e34613d51aeef04d26e02c6f5869903d59ae
AashrithG <gummadiaashrith@gmail.com>
2025-07-15 19:31:51 -0400

    Changed comments that contained non-ascii characters
    The checkstyle was having problems with it and it failed on generate docs in actions

--- 2025-SummerActivity/src/main/java/frc/robot/RobotContainer.java
+++ 2025-SummerActivity/src/main/java/frc/robot/RobotContainer.java
@@ -123,7 +123,7 @@ public class RobotContainer {
             () -> -controller.getLeftX(),
             () -> -controller.getRightX()));
 
-    // Lock to 0° when A button is held
+    // Lock to 0 when A button is held
     controller
         .a()
         .whileTrue(
@@ -136,7 +136,7 @@ public class RobotContainer {
     // Switch to X pattern when X button is pressed
     controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
-    // Reset gyro to 0° when B button is pressed
+    // Reset gyro to 0 when B button is pressed
     controller
         .b()
         .onTrue(
