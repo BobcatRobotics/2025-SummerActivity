224e371486da97da8929be030e02d087964a9001
AashrithG <gummadiaashrith@gmail.com>
2025-07-15 19:42:46 -0400

    resolving document generation errors
    ascii encoding in comment is causing errors

--- 2025-SummerActivity/src/main/java/frc/robot/BuildConstants.java
+++ 2025-SummerActivity/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-SummerActivity";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 18;
-  public static final String GIT_SHA = "f1826790f957650fd053335a8f8dd9422644ba78";
-  public static final String GIT_DATE = "2025-07-15 18:33:38 EDT";
+  public static final int GIT_REVISION = 20;
+  public static final String GIT_SHA = "e9c7e34613d51aeef04d26e02c6f5869903d59ae";
+  public static final String GIT_DATE = "2025-07-15 19:31:51 EDT";
   public static final String GIT_BRANCH = "aashriths_branch";
-  public static final String BUILD_DATE = "2025-07-15 19:16:56 EDT";
-  public static final long BUILD_UNIX_TIME = 1752621416439L;
+  public static final String BUILD_DATE = "2025-07-15 19:35:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1752622519391L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
