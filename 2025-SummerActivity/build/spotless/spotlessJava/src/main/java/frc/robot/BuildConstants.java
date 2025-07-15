ae7c4c4fa1cf077ae32b61cd6e54030ce30a06d3
AashrithG <gummadiaashrith@gmail.com>
2025-07-15 19:20:38 -0400

    added to the build.gradle and changed checkstyle
    added to the build.gradle to fix the error where the checkstyle file was not found and changed the checkstyle file to fix a weird indentation error that shouldn't happen

--- 2025-SummerActivity/build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ 2025-SummerActivity/build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-SummerActivity";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "e4aeae3aeb3729db206a0aa8bbfe6535e5b6230e";
-  public static final String GIT_DATE = "2025-07-10 19:10:55 EDT";
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "f1826790f957650fd053335a8f8dd9422644ba78";
+  public static final String GIT_DATE = "2025-07-15 18:33:38 EDT";
   public static final String GIT_BRANCH = "aashriths_branch";
-  public static final String BUILD_DATE = "2025-07-10 19:18:16 EDT";
-  public static final long BUILD_UNIX_TIME = 1752189496361L;
+  public static final String BUILD_DATE = "2025-07-15 19:16:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1752621416439L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
