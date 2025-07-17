3dcf41843e0796111b35d759893613401f89fbba
GIRPRA <girishprasad102@gmail.com>
2025-07-17 18:22:22 -0400

    Added tuner constants

--- 2025-SummerActivity/build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ 2025-SummerActivity/build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-SummerActivity";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "6c0e388e1568a96a8981e368009e26756b17ad9f";
-  public static final String GIT_DATE = "2025-07-10 19:36:04 EDT";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "a5296aa96b5451c831448fe554383d34b7133166";
+  public static final String GIT_DATE = "2025-07-15 19:14:27 EDT";
   public static final String GIT_BRANCH = "Girish_Branch";
-  public static final String BUILD_DATE = "2025-07-15 18:44:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1752619450389L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-07-17 18:19:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1752790790481L;
+  public static final int DIRTY = 128;
 
   private BuildConstants() {}
 }
