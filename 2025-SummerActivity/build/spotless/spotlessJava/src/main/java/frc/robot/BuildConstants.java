158698ec2cc72cfc8df9a95dd4b3df488530e0bb
GIRPRA <girishprasad102@gmail.com>
2025-07-24 19:43:13 -0400

    Girish, Sri, and Rishi Updated kMaxSpeedAt12Volts and kSlipCurrent

--- 2025-SummerActivity/build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ 2025-SummerActivity/build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-SummerActivity";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 16;
-  public static final String GIT_SHA = "a5296aa96b5451c831448fe554383d34b7133166";
-  public static final String GIT_DATE = "2025-07-15 19:14:27 EDT";
+  public static final int GIT_REVISION = 19;
+  public static final String GIT_SHA = "19870724328ae0abb1e72f7706e5125c4edd4f8c";
+  public static final String GIT_DATE = "2025-07-17 18:30:50 EDT";
   public static final String GIT_BRANCH = "Girish_Branch";
-  public static final String BUILD_DATE = "2025-07-17 18:19:50 EDT";
-  public static final long BUILD_UNIX_TIME = 1752790790481L;
+  public static final String BUILD_DATE = "2025-07-23 09:24:27 EDT";
+  public static final long BUILD_UNIX_TIME = 1753277067661L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
